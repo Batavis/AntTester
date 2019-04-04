f3fa7db5bf9a4df678b68adc2c80850e521e5e7e
Batavis <kovacs@fim.uni-passau.de>
2019-04-04 19:40:08 +0200

    set different string in branch-b

--- src/AntTester.java
+++ src/AntTester.java
@@ -6,7 +6,7 @@ public class AntTester {
 	public static void main(String[] args) {
 		int k = (int) random(100);
 		for (int i = 0; i<sqrt(k);i++) {
-			if (i<5) System.out.println("<=25");
+			if (i<5) System.out.println("absolute difference <=25 in branch-b");
 		}
 	}
 
