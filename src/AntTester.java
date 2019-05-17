b4856b5e6ba19dea8fe42fcb206cf8ede89f6eb9
Batavis <kovacs@fim.uni-passau.de>
2019-05-17 12:31:07 +0200

    property fixed at a

--- src/AntTester.java
+++ src/AntTester.java
@@ -6,7 +6,8 @@ public class AntTester {
 	public static void main(String[] args) {
 		int k = (int) random(100);
 		for (int i = 0; i<sqrt(k);i++) {
-			if (i<5) System.out.println("difference <=25 for branch-a with Junit tests");
+			if (i<5) System.out.println("difference <=25 for branch-a with proper property
+		");
 		}
 	}
 	public int product(int factor1, int factor2) {
