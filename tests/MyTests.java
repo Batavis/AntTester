e0e1e957879c0923b4f193e376bcf6b79db3f704
Batavis <kovacs@fim.uni-passau.de>
2019-09-06 11:14:26 +0200

    working compile

---
+++ tests/MyTests.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import java.net.URL;
+import java.net.URLClassLoader;
+public class MyTests {
+	
+	public static void main(String[] args) {
+		ClassLoader cl = ClassLoader.getSystemClassLoader();
+
+        URL[] urls = ((URLClassLoader)cl).getURLs();
+
+        for(URL url: urls){
+        	System.out.println(url.getFile());
+        }
+	}
+    @Test
+	public void testProductWithZero() {
+		AntTester tester = new AntTester();
+		
+		//test to pass
+		assertEquals(0, tester.product(10, 0));
+		
+		//test to fail
+		//assertEquals(0, tester.product8(10,10));
+	}
+	
+	@Test
+	public void testString() {
+		assertEquals("", "0");
+	}
+	
+}
