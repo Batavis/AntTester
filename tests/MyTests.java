00ed1d4680c4046297d03edbfa238ef4e18820dd
Florian Kovacs <kovacs@fim.uni-passau.de>
2020-02-06 12:55:26 +0100

    cleanup master

--- tests/MyTests.java
+++ tests/MyTests.java
@@ -1,19 +1,8 @@
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import java.net.URL;
-import java.net.URLClassLoader;
 public class MyTests {
 	
-	public static void main(String[] args) {
-		ClassLoader cl = ClassLoader.getSystemClassLoader();
-
-        URL[] urls = ((URLClassLoader)cl).getURLs();
-
-        for(URL url: urls){
-        	System.out.println(url.getFile());
-        }
-	}
     @Test
 	public void testProductWithZero() {
 		AntTester tester = new AntTester();
