ad2c35fca290171c371e6f360748e7b760cc8052
Batavis <kovacs@fim.uni-passau.de>
2020-02-25 10:52:52 +0100

    add second test class

---
+++ tests/Tests2.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.Ignore;
+
+public class Tests2 {
+	@Test
+	public void testMainClass() {
+		assertEquals(4, new AntTester().product(2,2));
+	}
+	
+}
